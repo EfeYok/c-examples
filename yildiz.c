e8191b1fceb63db698c4dc1d492832266286f9f7
EfeYok <251109030@stu.kmu.edu.tr>
2026-04-01 02:01:39 +0300

    Yıldız merdiven ödevi

---
+++ yildiz.c
@@ -0,0 +1,15 @@
+// Alt alta 10 Yıldız~
+
+#include <stdio.h>
+
+int main() {
+  int y = 5; // y değişkeni tanımlandı.
+  for (int i = y; i >= 1; i--) {
+    for (int e = y; e >= i; e--) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
